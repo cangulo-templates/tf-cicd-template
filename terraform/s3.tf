3f76f03fabdc51e1e3efc2c6acd6c125ffd62f7c
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-10-08 21:13:46 +0200

    test(gh): [ci] test terraform security check using aquasecurity/trivy-action (#6)

---
+++ terraform/s3.tf
@@ -0,0 +1,13 @@
+locals {
+  buckets = {
+    main = {
+      bucket = "${local.resources_prefix}-data"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "this" {
+  for_each = local.buckets
+
+  bucket = each.value.bucket
+}
