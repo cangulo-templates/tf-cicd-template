5b077b3ad6fb34c42db3753315e74c6e80fc6f95
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2023-10-08 21:50:47 +0200

    test(gh): [ci] fixed some security alerts

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,11 @@
 locals {
   buckets = {
     main = {
-      bucket = "${local.resources_prefix}-data"
+      bucket                  = "${local.resources_prefix}-data"
+      block_public_acls       = true
+      block_public_policy     = true
+      ignore_public_acls      = true
+      restrict_public_buckets = true
     }
   }
 }
@@ -10,4 +14,18 @@ resource "aws_s3_bucket" "this" {
   for_each = local.buckets
 
   bucket = each.value.bucket
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  for_each = local.buckets
+
+  bucket                  = each.value.bucket
+  block_public_acls       = each.value.block_public_acls
+  block_public_policy     = each.value.block_public_policy
+  ignore_public_acls      = each.value.ignore_public_acls
+  restrict_public_buckets = each.value.restrict_public_buckets
+
+  depends_on = [
+    aws_s3_bucket.this
+  ]
 }
